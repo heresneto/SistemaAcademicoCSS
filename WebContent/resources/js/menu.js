a4bf6320004b6bf776f7f1bea501199cd253a198
Heres Neto <heresneto@gmail.com>
2017-10-23 11:07:51 -0300

    ATUALIZACAO MANEIRA

--- WebContent/resources/js/menu.js
+++ WebContent/resources/js/menu.js
@@ -1,12 +1,8 @@
 jQuery(document).ready(function($) {
 
-	$(".mostraEsquerda").click(function() {
-		$(".menuLeft").fadeToggle();
-		$(".mostraEsquerda").toggleClass("fecha");
-	});
-
 	$(".iconeMenu").click(function() {
+		$(".menu").fadeToggle();
 		$(this).toggleClass("fecha");
-	})
+	});
 
 });
